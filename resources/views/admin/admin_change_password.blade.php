2a3e66cd9914bca962721d421cd1045f10ff5f37
BrivaHamisi <brivahamisi@gmail.com>
2023-05-10 14:05:55 +0300

    Admin Change Password Succesfully

--- resources/views/admin/admin_change_password.blade.php
+++ resources/views/admin/admin_change_password.blade.php
@@ -4,11 +4,19 @@
     <div class="page-content">
         <div class="container-fluid">
 
-                <div class="col-10">
+                <div class="col-12">
                     <div class="card">
                         <div class="card-body">
 
                             <h4 class="card-title">Change Password Page</h4>
+
+                            @if(count($errors))
+                                @foreach($errors->all() as $error)
+                                    <p class="alert alert-danger alert-dismissible fade show" role="alert">{{$error}}</p>
+                                @endforeach
+                            @endif
+
+
                             <p class="card-title-desc">This form is pre-filled with your <code class="highlighter-rouge">Details. </code> Click to Edit </p>
 
                             <form method="POST" action="{{route('update.password')}}">
@@ -17,21 +25,21 @@
                                     <div class="row mb-3">
                                         <label for="example-text-input" class="col-sm-2 col-form-label">Old Password</label>
                                         <div class="col-sm-10">
-                                            <input class="form-control" name="oldpassword" id="oldpassword" type="text" id="example-text-input">
+                                            <input class="form-control" name="oldpassword" id="oldpassword" type="password" id="example-text-input">
                                         </div>
                                     </div>
                                     <!-- end row -->
                                     <div class="row mb-3">
                                         <label for="example-search-input" class="col-sm-2 col-form-label">New Password</label>
                                         <div class="col-sm-10">
-                                            <input class="form-control" name="newpassword" id="newpassword"  type="text" id="example-search-input">
+                                            <input class="form-control" name="newpassword" id="newpassword"  type="password" id="example-search-input">
                                         </div>
                                     </div>
                                     <!-- end row -->
                                     <div class="row mb-3">
                                         <label for="example-email-input" class="col-sm-2 col-form-label">Confirm Password</label>
                                         <div class="col-sm-10">
-                                            <input class="form-control" name="confirmpassword" id="confirmpassword" type="email" id="example-email-input">
+                                            <input class="form-control" name="confirmpassword" id="confirmpassword" type="password" id="example-email-input">
                                         </div>
                                     </div>
                                     <!-- end row -->
